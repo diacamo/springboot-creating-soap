f2dc192f78ae48082aab355e3ea3a5755a6300ce
Diacamo António Mankungi <diacamo.antonio.mankungi@atlantico.ao>
2020-06-26 00:58:45 +0100

    add file projects

---
+++ src/main/java/com/diacamo/CreatingSoapApplication.java
@@ -0,0 +1,13 @@
+package com.diacamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingSoapApplication.class, args);
+    }
+
+}
